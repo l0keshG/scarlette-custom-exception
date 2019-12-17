3f463492285ce4ec860972f739e36e232a2b0253
Lokesh G <45425189+l0keshG@users.noreply.github.com>
2019-12-17 18:52:27 +0530

    using HTTPException

--- error.py
+++ error.py
@@ -1,25 +1,13 @@
+"""
+Errors raised by the application
+"""
+from starlette.exceptions import HTTPException
 
-class BaseError(Exception):
-    """
-    Base class for other errors to extend
-    """
-    status_code = None
-    message = None
-
-    def __init__(self, status_code, description):
-        self.description = description
-        self.status_code = status_code
-        super(BaseError, self).__init__(status_code, description)
-
-    def __str__(self):
-        return self.description
-
-
-class EmptyParamError(BaseError):
+class EmptyParamError(HTTPException):
     """ Error is raised when group name is not provided. """
     status_code = 400
     message = "Missing Group Name"
 
-class InvalidUsage(BaseError):
+class InvalidUsage(HTTPException):
     status_code = 400
-    message = "ajkashjkashkdj"
+    message = "Invalid usage of the method"
